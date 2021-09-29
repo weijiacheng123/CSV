b31371d06f8642d517c95afce5d87acd6b556989
Weijia Cheng <weijia_cheng1@baylor.edu>
2021-09-29 16:28:13 -0500

    add death valley

--- sitka4.py
+++ sitka4.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
@@ -13,9 +13,10 @@ print(type(header_row))
 for index, column_header in enumerate(header_row):
     print(index, column_header)
 
+
 # testing to convert date from string
-mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
-print(type(mydate))
+# mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
+# print(type(mydate))
 
 
 highs = []
@@ -23,22 +24,32 @@ dates = []
 lows = []
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    the_date = datetime.strptime(row[2], '%Y-%m-%d')
-    dates.append(the_date)
+    try:
+        the_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+    except ValueError:
+        print(f"Missing data for {the_date}")
+    else:
+        highs.append(int(row[4]))
+        lows.append(int(row[5]))
+        dates.append(the_date)
+# missing data use except method.
 
-print(highs)
-print(dates)
 
+# print(highs)
+# print(dates)
+# commen that can show like list
+
+'''
 fig = plt.figure()
 
-'''
-for row in csv_file:
-    highs.append(int(row[5]))
 
-print(highs)
-'''
+#for row in csv_file:
+    #highs.append(int(row[5]))
+
+#print(highs)
+
 
 plt.title("Daliy high temperatures, July 2018", fontsize=16)
 plt.xlabel("", fontsize=12)
@@ -65,3 +76,4 @@ plt.title("Lows")
 
 plt.suptitle("Highs and Lows of Sitka, Alaska")
 plt.show()
+'''
