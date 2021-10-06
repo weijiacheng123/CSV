9aa3ece5b459944a9aaba69bda2555460bccf485
weijiacheng123 <89552558+weijiacheng123@users.noreply.github.com>
2021-10-06 17:37:57 -0500

    Update sitka5.py

--- sitka5.py
+++ sitka5.py
@@ -22,18 +22,21 @@ for index, column_header in enumerate(header_row):
 highs = []
 dates = []
 lows = []
+name1 = []
 
 for row in csv_file:
     try:
         the_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[4])
         low = int(row[5])
+        name1 = row[1]
     except ValueError:
         print(f"Missing data for {the_date}")
     else:
         highs.append(int(row[4]))
         lows.append(int(row[5]))
         dates.append(the_date)
+        name1.append(row[1])
 # missing data use except method.
 
 
@@ -85,13 +88,23 @@ print(type(mydate))
 highs = []
 dates = []
 lows = []
+name2 = []
 
 for row in csv_file1:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    the_date = datetime.strptime(row[2], '%Y-%m-%d')
-    dates.append(the_date)
-
+     try:
+        the_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        name2 = row[1]
+    except ValueError:
+        print(f"Missing data for {the_date}")
+    else:
+        highs.append(int(row[5]))
+        lows.append(int(row[6]))
+        the_date = datetime.strptime(row[2], '%Y-%m-%d')
+        dates.append(the_date)
+        name2.append(row[1])
+  
 print(highs)
 print(dates)
 
